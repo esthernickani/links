ef3002722494a0e39df60e919dcbeed3e3ea730a
esthernickani <nickaniesther@gmail.com>
2024-05-10 21:28:52 -0600

    complete styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
+    /*copying shortened link to clipboard*/
+
+    const copyBtns = document.querySelectorAll('.copy_link')
+    const link = document.querySelector('.link_to_copy')
+
+    const copyLink = async (e) => {
+        e.preventDefault()
+        console.log(e.target.parentElement)
+        let text = e.target.parentElement.previousElementSibling.querySelector('.link_to_copy').innerText
+        console.log(text)
+
+        try {
+            await navigator.clipboard.writeText(text);
+            /*change text and color of button*/
+            console.log(e.target.parentElement)
+            e.target.parentElement.innerHTML = 'Copied!'
+            console.log('Copied to clipboard')
+        } catch(err) {
+            console.log('Failed to copy')
+        }
+    }
+
+    copyBtns.forEach(
+        copyBtn => copyBtn.addEventListener('click', copyLink)
+    )
     /*const submitLinkBtn = document.querySelector("#form-submit")
 
     async function shortenLink (e) {
